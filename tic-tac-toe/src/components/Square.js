8c8786628767fa3bf368be2672f02fa147ac16ee
satyashinedezign <satyabeer@shinedezign.com>
2022-02-01 11:40:29 +0530

    added square box

---
+++ tic-tac-toe/src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/Square.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square"></button>
+        )
+    }
+}
+
+export default Square;
